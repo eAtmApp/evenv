473a2e00b6be8cf57d73143f8795c6c2e55b142c
eATM <eatm.app@gmail.com>
2024-05-26 22:58:14 +0800

    aaa

--- eVirtualEnv.cpp
+++ eVirtualEnv.cpp
@@ -4,16 +4,39 @@
 #include <iostream>
 #include <easylib.h>
 #include "util.hpp"
+#include <type_traits>
 
 evenv ev;
+
+/*
+std::ostream& operator<<(std::ostream& os, const env_item& d)
+{
+	return os << "XXX";
+}*/
+
 
 int main()
 {
 	ev.del_evenv_envpath();
+
+	//static_assert(is_equality_comparable<env_item>::value, "不支持流");
+
+	console.log("进入evenv...");
 	if (ev.is_agent_run())
 	{
-
+		console.log("代理运行...");
+		ev.del_evenv_envpath();
+		VERIFY(0, "代理运行.");
 	}
+	else {
+		console.log("正常运行");
+	}
+
+	process.auto_proxy_input([](eString& cmdStr)
+		{
+
+			return true;
+		});
 
 	return 0;
 }
--- util.hpp
+++ util.hpp
@@ -2,43 +2,106 @@
 
 using namespace easy;
 
+struct env_item {
+	std::string name;
+	std::string path;
+/*
+	bool operator==(const env_item& other) const {
+		return true;
+	}*/
+};
+
 class evenv
 {
 private:
+	eString _exe_name;
+	eString _exe_dir;
 
+	bool	_is_agent_run = false;
+
+	//conda虚拟环境
+	eVector<env_item> _envs_conda;
+	//eStringArray
 public:
 	evenv()
 	{
+		_exe_name = process.exe_name().tolower();
+		_exe_dir = process.exe_dir().tolower();
 
+		//判断是否代理运行
+		//正常运行:命令行参数为全路径,可执行文件名为evenv,当前目录为可执行程序所在目录的
+		{
+			if (_exe_name != "evenv"
+				&& _exe_dir.compare_icase(process.cwd()))
+			{
+				_is_agent_run = true;
+			}
+		}
 	}
+
+	//读取conda虚拟环境
+	bool	read_conda_envs()
+	{
+		eString conda_env_file = process.get_env("USERPROFILE");
+		if (conda_env_file.empty()) conda_env_file = process.get_env("HOMEDRIVE") + process.get_env("USERPROFILE");
+		VERIFY(!conda_env_file.empty(), "获取用户文件夹失败!");
+		conda_env_file += "\\.conda\\environments.txt";
+		if (!util::is_file_exists(conda_env_file))
+		{
+			return false;
+		}
+	}
+
 	//判断是否代理运行
-	//以下情况为正常运行:
-	//*******************命令行参数为全路径
-	//*******************可执行文件名为evenv
-	//*******************当前目录为可执行程序所在目录的
 	bool	is_agent_run()
 	{
-		eString extName = process.exe_name().tolower();
-		if (extName == "evenv") return false;
-
-		eString exeDir = process.exe_dir();
-		eString curDir = process.cwd();
-		if (exeDir.compare_icase(curDir)) return false;
-
-		return false;
+		return _is_agent_run;
 	}
 
 	//将event所在目录从进程环境变量path中删除
 	void	del_evenv_envpath()
 	{
 		eString pathStr = process.get_env("PATH");
-		auto pathItems = pathStr.split(";");
+		pathStr = delete_evenv_path_string(pathStr);
 		process.set_env("PATH", pathStr);
 	}
 
-private:
-	eString	deleteCwdPath(eString &pathStr)
+	//运行命令
+	void	run()
 	{
+		do
+		{
+			std::cout << "设置当前目录默认环境变量:\r\n";
 
+		} while (true);
+	}
+
+private:
+
+	//删除环境变量字符串中的evenv目录
+	eString	delete_evenv_path_string(eString& pathStr)
+	{
+		eString exeDir = process.exe_dir().tolower();
+
+		auto pathItems = pathStr.split_string(";");
+		eStringArray arrayItems;
+		pathItems.forEach([&](eString& oldStr)
+			{
+				if (oldStr.empty()) return;
+
+				eString tmpStr(oldStr);
+				tmpStr.tolower();
+				tmpStr.replaceAll("\\\\", "\\");
+				tmpStr.replaceAll("/", "\\");
+				if (tmpStr.back() != '\\') tmpStr += "\\";
+
+				if (exeDir != tmpStr)
+				{
+					arrayItems.push_back(oldStr);
+				}
+
+			});
+
+		return arrayItems.join(";");
 	}
 };
